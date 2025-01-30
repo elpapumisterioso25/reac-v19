afade2c0d9f0fe803078861379a1d56a9acb9e58
elpapumisterioso25 <efraingg917@gmail.com>
2025-01-30 08:17:13 -0500

    primer commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState, version } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import PantallaApi from './pantallaApi';
+
+function App() {
+  return (
+    <>
+    
+      <PantallaApi />
+    </>
+  )
+}
+
+export default App;
